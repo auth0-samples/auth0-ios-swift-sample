00868d41e4e8833707afd3eada7c673e151299ba
Rita Zerrizuela <zeta@widcket.com>
2022-05-14 02:51:30 -0300

    Rename test methods

--- Sample-01/Tests/Shared/UserTests.swift
+++ Sample-01/Tests/Shared/UserTests.swift
@@ -13,7 +13,7 @@ extension User: Equatable {
 }
 
 class UserTests: XCTestCase {
-    func testReturnsEmptyProfile() {
+    func testReturnsEmptyUser() {
         let sut = User.empty
         XCTAssertEqual(sut.id, "")
         XCTAssertEqual(sut.name, "")
@@ -23,7 +23,7 @@ class UserTests: XCTestCase {
         XCTAssertEqual(sut.updatedAt, "")
     }
 
-    func testReturnsProfileFromIDToken() {
+    func testReturnsUserFromIDToken() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28iLCJuYW1lIjoiYmFyIiwiZW1haWwiOiJmb29AZXhhbXB"
             + "sZS5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGljdHVyZSI6ImJheiIsInVwZGF0ZWRfYXQiOiJxdXgifQ.vc9sxvhUVAHowIWJ"
             + "7D_WDzvqJxC4-qYXHmiBVYEKn9E"
@@ -36,44 +36,44 @@ class UserTests: XCTestCase {
         XCTAssertEqual(sut.updatedAt, "qux")
     }
 
-    func testReturnsEmptyProfileWhenIDTokenDecodingFails() {
+    func testReturnsEmptyUserWhenIDTokenDecodingFails() {
         XCTAssertEqual(User.from("foo.bar.baz"), User.empty)
     }
 
-    func testReturnsEmptyProfileWhenSubjectIsMissing() {
+    func testReturnsEmptyUserWhenSubjectIsMissing() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYmFyIiwiZW1haWwiOiJmb29AZXhhbXBsZS5jb20iLCJlbWF"
             + "pbF92ZXJpZmllZCI6dHJ1ZSwicGljdHVyZSI6ImJheiIsInVwZGF0ZWRfYXQiOiJxdXgifQ.dkS2qn1pbmznis5krUlKuornFIr-lZ_v"
             + "TDn36ksFzFM"
         XCTAssertEqual(User.from(idToken), User.empty)
     }
 
-    func testReturnsEmptyProfileWhenNameIsMissing() {
+    func testReturnsEmptyUserWhenNameIsMissing() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28iLCJlbWFpbCI6ImZvb0BleGFtcGxlLmNvbSIsImVtYWl"
             + "sX3ZlcmlmaWVkIjp0cnVlLCJwaWN0dXJlIjoiYmF6IiwidXBkYXRlZF9hdCI6InF1eCJ9.92bnGEfJGKt5vsdkFHeyeykrlynn4J6tSR"
             + "w9ex2XsqE"
         XCTAssertEqual(User.from(idToken), User.empty)
     }
 
-    func testReturnsEmptyProfileWhenEmailIsMissing() {
+    func testReturnsEmptyUserWhenEmailIsMissing() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28iLCJuYW1lIjoiYmFyIiwiZW1haWxfdmVyaWZpZWQiOnR"
             + "ydWUsInBpY3R1cmUiOiJiYXoiLCJ1cGRhdGVkX2F0IjoicXV4In0.g9zSyuxzfNlN_-6E1FJfJdQhGpPMTLI0W8hIaiyylng"
         XCTAssertEqual(User.from(idToken), User.empty)
     }
 
-    func testReturnsEmptyProfileWhenEmailVerifiedIsMissing() {
+    func testReturnsEmptyUserWhenEmailVerifiedIsMissing() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28iLCJuYW1lIjoiYmFyIiwiZW1haWwiOiJmb29AZXhhbXB"
             + "sZS5jb20iLCJwaWN0dXJlIjoiYmF6IiwidXBkYXRlZF9hdCI6InF1eCJ9.OpiRejUet5bC2-ea4AtTQp7PI1kvDHM_lGLKLyrM5Z0"
         XCTAssertEqual(User.from(idToken), User.empty)
     }
 
-    func testReturnsEmptyProfileWhenPictureIsMissing() {
+    func testReturnsEmptyUserWhenPictureIsMissing() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28iLCJuYW1lIjoiYmFyIiwiZW1haWwiOiJmb29AZXhhbXB"
             + "sZS5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwidXBkYXRlZF9hdCI6InF1eCJ9.A1BwdpsSf3azr8I724tVdz0iWA9qrVwFOHOf_D"
             + "WekQY"
         XCTAssertEqual(User.from(idToken), User.empty)
     }
 
-    func testReturnsEmptyProfileWhenUpdatedAtIsMissing() {
+    func testReturnsEmptyUserWhenUpdatedAtIsMissing() {
         let idToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28iLCJuYW1lIjoiYmFyIiwiZW1haWwiOiJmb29AZXhhbXB"
             + "sZS5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGljdHVyZSI6ImJheiJ9.mHTo0v7jIVQ9ELcstaDV3FxSUsXr4IMejKRY0e2MQ9"
             + "Y"
