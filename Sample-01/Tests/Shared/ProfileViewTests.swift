999e9199404b48d7c9b87dca58bbc4fa7d524418
Rita Zerrizuela <zeta@widcket.com>
2022-07-07 19:58:47 -0300

    Simplify implementation

--- Sample-01/Tests/Shared/ProfileViewTests.swift
+++ Sample-01/Tests/Shared/ProfileViewTests.swift
@@ -7,15 +7,14 @@ extension ProfileView: Inspectable {}
 
 class ProfileViewTests: XCTestCase {
     func testHasHeader() throws {
-        let binding = Binding<User>(wrappedValue: User.empty)
-        let sut = ProfileView(user: binding)
+        let user = User(id: "", name: "", email: "", emailVerified: "", picture: "", updatedAt: "")
+        let sut = ProfileView(user: Binding<User>(wrappedValue: user))
         XCTAssertNotNil(try? sut.inspect().list().find(ProfileHeader.self))
     }
 
     func testHasProfileValues() throws {
         let user = User(id: "foo", name: "bar", email: "baz", emailVerified: "qux", picture: "", updatedAt: "quux")
-        let binding = Binding<User>(wrappedValue: user)
-        let sut = ProfileView(user: binding)
+        let sut = ProfileView(user: Binding<User>(wrappedValue: user))
         let cells = try sut.inspect().list().findAll(ProfileCell.self)
         XCTAssertEqual(cells.count, 5)
         XCTAssertEqual(try cells[0].findAll(ViewType.Text.self).last?.string(), user.id)
