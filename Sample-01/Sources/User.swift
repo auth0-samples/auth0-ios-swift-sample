1d4007a2cb6bb34d12f5e158fc5864e29c19a1c6
Rita Zerrizuela <zeta@widcket.com>
2022-07-27 08:01:05 -0300

    Use the new JWTDecode claims subscript (#100)

--- Sample-01/Sources/User.swift
+++ Sample-01/Sources/User.swift
@@ -13,11 +13,11 @@ extension User {
     init?(from idToken: String) {
         guard let jwt = try? decode(jwt: idToken),
               let id = jwt.subject,
-              let name = jwt.claim(name: "name").string,
-              let email = jwt.claim(name: "email").string,
-              let emailVerified = jwt.claim(name: "email_verified").boolean,
-              let picture = jwt.claim(name: "picture").string,
-              let updatedAt = jwt.claim(name: "updated_at").string else {
+              let name = jwt["name"].string,
+              let email = jwt["email"].string,
+              let emailVerified = jwt["email_verified"].boolean,
+              let picture = jwt["picture"].string,
+              let updatedAt = jwt["updated_at"].string else {
             return nil
         }
         self.id = id
